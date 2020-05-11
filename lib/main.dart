f1926c27f91f9febed99299f87a13134fa33de0f
Kanat Bektursyn <36327338+kanatBektursyn@users.noreply.github.com>
2020-05-12 02:34:34 +0500

    foodCard

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,58 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var FoodCard = Padding(
+      padding: EdgeInsets.all(12),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[100],
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              spreadRadius: 3,
+              blurRadius: 10,
+              offset: Offset(2, 3), // changes position of shadow
+            ),
+          ],
+        ), // Yellow
+        height: 200,
+        width: 130,
+        alignment: Alignment.center,
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 30),
+            RichText(
+              text: TextSpan(
+                text: "🍕",
+                style: TextStyle(
+                  fontSize: 35,
+                ),
+              ),
+            ),
+            Spacer(),
+            RichText(
+              text: TextSpan(
+                text: "Pizza",
+                style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.black54,
+                    fontFamily: "Montserrat",
+                    fontWeight: FontWeight.w600),
+              ),
+            ),
+            Spacer(),
+            Icon(
+              Icons.code,
+              size: 30,
+              color: Colors.black54,
+            ),
+            SizedBox(height: 30),
+          ],
+        ),
+      ),
+    );
+
     var HeaderDetails = Column(
       children: <Widget>[
         Row(
@@ -49,7 +101,7 @@ class MyApp extends StatelessWidget {
                     text: "Delivery",
                     style: TextStyle(
                       fontSize: 45,
-                      fontWeight: FontWeight.w500,
+                      fontWeight: FontWeight.w600,
                       color: Colors.black.withOpacity(0.8),
                     ),
                   ),
@@ -90,13 +142,39 @@ class MyApp extends StatelessWidget {
                     ),
                     Row(
                       children: <Widget>[
-                        Icon(
-                          Icons.search,
-                          size: 30,
-                          color: Colors.black38,
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                          child: Icon(
+                            Icons.search,
+                            size: 35,
+                            color: Colors.black38,
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Expanded(
+                          child: TextField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              labelText: 'Search...',
+                            ),
+                          ),
                         ),
                       ],
-                    )
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: <Widget>[
+                          FoodCard,
+                          FoodCard,
+                          FoodCard,
+                          FoodCard,
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
---
+++ widgets/food_card.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void FoodCard() {
+  return Container(
+    child: Column(
+      children: <Widget>[
+        Text(":pizza:"),
+      ],
+    ),
+  );
+}
